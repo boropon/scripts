cc48f466e16d30954bdcac135a0e799c9248e727
boropon <coco_mix_620@yahoo.co.jp>
2014-06-09 00:41:42 +0900

    Add function for .identifier.

--- add_simgps_penvvalue/add_simgps_penvvalue.rb
+++ add_simgps_penvvalue/add_simgps_penvvalue.rb
@@ -31,19 +31,45 @@ def getGpsPenvVariableAndValue(gpsdefstring)
 	return hash
 end
 
+def integer_string?(str)
+	Integer(str)
+	true
+rescue ArgumentError
+	false
+end
+
 machine_and_dest = ARGV[0]
-penvvar_filepath = "/proj/lpux/products/" + ARGV[0] + "/base/gw_printer/p_gpslib/include/gps/penv_var.h"
+#penvvar_filepath = "/proj/lpux/products/" + ARGV[0] + "/base/gw_printer/p_gpslib/include/gps/penv_var.h"
+penvvar_filepath = "./penv_var.h"
 
 # .identifierに対する処理
 all_variables = getGrepStrings(penvvar_filepath, "GPS_PENV_VAR_ID")
 all_variables.each { |str|
+	
 	hash = getGpsPenvVariableAndValue(str)
-	# valueが文字列なら再検索して数値に変換
 	search_result = getGrepStrings("./.identifier", hash["variable"]+"\t")
-
 	if search_result.size == 0
-		# .identifierに定義を追加(variable, value両方)
+		# .identifierに定義を追加(VAR, VAL両方)
+		
+		
+		# VAR, VAL共通の値取得処理
+		while str.size != 0
+			hash = getGpsPenvVariableAndValue(str)
+			if integer_string?(hash["value"])
+				envvalue = hash["value"]
+				break
+			else
+				/(\()(.+)(\))/ =~ hash["value"]
+				if !$2.nil? then
+					envvalue = $2
+					break
+				else
+					str = getGrepStrings(penvvar_filepath, hash["value"]+"\t").first
+				end
+			end
+		end
 	end
+
 }
 
 # .variableに対する処理
